0c36ce00c34468ac99d95e80e878a25cc56f04de
5MatheusFraga7 <fragamatheus823@gmail.com>
2024-04-24 02:26:35 -0300

    Normalizando conexão com banco

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"sales-reports-service/internal/db"
+	"sales-reports-service/internal/db/adapters"
+)
+
 func main() {
 
+	// Conexão com banco
+
+	postgresAdapter := adapters.NewPostgreSQLAdapter()
+	err := db.OpenConnectionToDatabase(postgresAdapter)
+
+	if err != nil {
+		panic(err)
+	}
 }
--- internal/db/database.go
+++ internal/db/database.go
@@ -10,6 +10,7 @@ type Database interface {
 func OpenConnectionToDatabase(database Database) error {
 	err := database.Connect()
 	if err != nil {
+		fmt.Println("EROOOOOOO!!!!!: %v", err)
 		return err
 	}
 
